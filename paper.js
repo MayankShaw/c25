ecc2ca599ac279175b38b8bbaf71ed2e7126c350
MayankShaw <80954305+MayankShaw@users.noreply.github.com>
2021-08-09 21:28:54 +0530

    Add files via upload

--- paper.js
+++ paper.js
@@ -1,19 +1,38 @@
-class Paper{
-    constructor(x,y,width,height){
-        var paper_options ={
-            restitution: 0.3,
-            friction:5,
-            density:1
-        }
-		this.image=loadImage("paper.png")
-        this.paper=Bodies.rectangle(x,y,width,height,paper_options);
-        World.add(world,this.box);
-this.width=width
-this.height=height
-    }
-    display () {
-        fill("black");
-        rectMode(CENTER);
-        rect(this.paper.position.x,this.paper.position.y,this.width,this.height);
-    }
+class paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:0.3,
+			friction:0,
+			density:1.2
+
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.image=loadImage("paper.png");
+		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;		
+
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(255,0,255)
+			imageMode(CENTER);
+			image(this.image, 0,0,this.r, this.r)
+			//ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
 }
